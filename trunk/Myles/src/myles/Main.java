7b122d94d212305460a0cc798eceb8de4b7118a0
dmcelectrico <dmcelectrico@cec05940-a2b8-11de-9953-9d61583e2ada>
2009-09-26 13:15:53 +0000

    actualización

---
+++ trunk/Myles/src/myles/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myles;
+import myles.servers.*;
+/**
+ *
+ * @author User
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try{
+            System.out.println("Conexión...");
+            VagosConnection vagos1 = new VagosConnection("ju4nk4r", "ninguna");
+            System.out.println(vagos1.Connect()+"\nDesconexión...");
+            System.out.println(vagos1.disConnect());
+        }
+        catch(java.net.MalformedURLException e){
+            System.out.println("MalformedURLException");
+        }
+        catch(java.lang.Exception e){
+            System.out.println("Excepcion");
+        }
+
+    }
+
+}
