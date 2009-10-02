371a41adbb2a449e4a12ae181e3d08b753d6b74a
dmcelectrico <dmcelectrico@cec05940-a2b8-11de-9953-9d61583e2ada>
2009-10-02 14:33:26 +0000

    Main revertido

--- trunk/Myles/src/myles/Main.java
+++ trunk/Myles/src/myles/Main.java
@@ -1,21 +1,34 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
+* To change this template, choose Tools | Templates
+* and open the template in the editor.
+*/
 package myles;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import myles.servers.*;
 /**
- *
- * @author Juankar
- */
+<<<<<<< .copia-de-trabajo
+<<<<<<< .mine
+*
+* @author Juankar
+*/
+
 public class Main {
+    public static void main(String[] args){
+        try {
+            /*ServerHandler master_servers = new ServerHandler(Config.get_login_info());
+            master_servers.Init();
+            System.out.println(master_servers);*/
+            VagosConnection v1 = new VagosConnection("ju4nk4r", "231d175a6f588a685bc28d34133fa5ca", "1");
+            System.out.println("Hemos conectado? "+v1.is_connected());
+            v1.connect();
+            System.out.println("Hemos conectado? "+v1.is_connected());
+            v1.search("prototype");
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-
 }
+
