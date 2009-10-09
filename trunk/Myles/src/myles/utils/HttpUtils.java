2103d859519e7433843fc80873165e8c9dcc7b10
dmcelectrico <dmcelectrico@cec05940-a2b8-11de-9953-9d61583e2ada>
2009-10-09 11:17:03 +0000

    método openBrowser

--- trunk/Myles/src/myles/utils/HttpUtils.java
+++ trunk/Myles/src/myles/utils/HttpUtils.java
@@ -6,12 +6,14 @@ import java.io.InputStreamReader;
 import java.net.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.awt.Desktop;
 /**
  *
  * @author User
  */
 public class HttpUtils {
-    public static HttpURLConnection setHeaders(String host, HttpURLConnection conn){
+
+    public static HttpURLConnection setHeaders(String host, HttpURLConnection conn) {
         conn.addRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
         conn.addRequestProperty("Accept-Language", "es-es,es;q=0.8,en-us;q=0.5,en;q=0.3");
         conn.addRequestProperty("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.7");
@@ -22,7 +24,8 @@ public class HttpUtils {
         conn.addRequestProperty("Host", host);
         return conn;
     }
-    public static String getPage(String desiredURL, String cookies, String host){
+
+    public static String getPage(String desiredURL, String cookies, String host) {
         try {
             URL vagos_login_url = new URL(desiredURL);
             try {
@@ -30,20 +33,20 @@ public class HttpUtils {
                 NetObj.setDoOutput(true);
                 NetObj = HttpUtils.setHeaders(host, NetObj);
                 NetObj.setRequestMethod("POST");
-                if(cookies != null){
-                        NetObj.addRequestProperty("Cookie", cookies);
+                if (cookies != null) {
+                    NetObj.addRequestProperty("Cookie", cookies);
                 }
                 BufferedReader page_o = new BufferedReader(new InputStreamReader(NetObj.getInputStream()));
                 String page_html = "";
                 String aux;
-                while(true){
+                while (true) {
                     aux = page_o.readLine();
-                    if(aux!=null){
-                        page_html += page_o.readLine()+"\n";
-                    }else{
+                    if (aux != null) {
+                        page_html += page_o.readLine() + "\n";
+                    } else {
                         break;
                     }
-            }
+                }
                 return page_html;
 
             } catch (IOException ex) {
@@ -58,4 +61,21 @@ public class HttpUtils {
         }
     }
 
+    /**
+     * Permite abrir una url en el navegador predeterminado de la maquina.
+     *
+     * @param url
+     * @throws java.io.IOException Lanzada si ocurren problemas de comunicación.
+     *                              También si la URL no tiene "http://"
+     * @throws java.net.URISyntaxException  URL incorrecta.
+     */
+    public static void openBrowser(String url) throws java.io.IOException, java.net.URISyntaxException {
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (java.io.IOException e) {
+            System.out.println("Error de comunicación");
+        } catch (java.net.URISyntaxException e) {
+            System.out.println("URL erronea");
+        }
+    }
 }
