483cd3d49ab077f5bd915c8de93f169c94f6399c
dmcelectrico <dmcelectrico@cec05940-a2b8-11de-9953-9d61583e2ada>
2009-10-10 14:57:25 +0000

    Solucionado disConnect. Sobraban lineas por el getHash(). Main actualizado con disConnect().

--- Myles/src/myles/Main.java
+++ Myles/src/myles/Main.java
@@ -10,11 +10,11 @@ import myles.servers.*;
 
 public class Main {
     public static void main(String[] args){
+        VagosConnection v1 = new VagosConnection("ju4nk4r", "231d175a6f588a685bc28d34133fa5ca", "1");
         try {
             /*ServerHandler master_servers = new ServerHandler(Config.get_login_info());
             master_servers.Init();
             System.out.println(master_servers);*/
-            VagosConnection v1 = new VagosConnection("ju4nk4r", "231d175a6f588a685bc28d34133fa5ca", "1");
             System.out.println("Hemos conectado? "+v1.is_connected()+" "+v1.user());
             v1.connect();
             System.out.println("Hemos conectado? "+v1.is_connected()+" "+v1.user());
@@ -22,6 +22,12 @@ public class Main {
 
         } catch (Exception ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } finally{
+            try {
+                v1.disConnect();
+            } catch (Exception e){
+                System.out.println("cohone, ni desconcta esto, que dice que "+e);
+            }
         }
     }
 }
