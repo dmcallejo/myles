82c0303ea7d5a903fd837dcd4c9d6a508e476ba4
dmcelectrico <dmcelectrico@cec05940-a2b8-11de-9953-9d61583e2ada>
2009-10-14 16:22:51 +0000

    Implementado rutas relativas + información de login de los xmls.

--- Myles/src/myles/Main.java
+++ Myles/src/myles/Main.java
@@ -3,7 +3,9 @@ package myles;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import myles.servers.*;
-import myles.search.*;
+import myles.config.*;
+import java.util.LinkedList;
+import org.jdom.Element;
 /**
 *
 * @author Juankar
@@ -11,23 +13,19 @@ import myles.search.*;
 
 public class Main {
     public static void main(String[] args){
-        VagosConnection v1 = new VagosConnection("ju4nk4r", "231d175a6f588a685bc28d34133fa5ca", "1");
         try {
-            int[] servers = {2,4};
+            LinkedList<Element> logins = Config.getLoginInfo();
+            Element log = logins.getLast();
+            VagosConnection v1 = new VagosConnection(log.getChildText("user"),log.getChildText("encpass"), "1");
+            int[] servers = {1,2};
             System.out.println("Hemos conectado? "+v1.is_connected()+" "+v1.user());
             v1.connect();
             System.out.println("Hemos conectado? "+v1.is_connected()+" "+v1.user());
             v1.search("prototype",servers);
-
+            
 
         } catch (Exception ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        } finally{
-            try {
-                v1.disConnect();
-            } catch (Exception e){
-                System.out.println("cohone, ni desconcta esto, que dice que "+e);
-            }
         }
     }
 }
