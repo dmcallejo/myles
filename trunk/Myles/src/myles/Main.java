c95422788b484e6549ef2095e434185ef90ab4e7
dmcelectrico <dmcelectrico@cec05940-a2b8-11de-9953-9d61583e2ada>
2010-02-22 11:55:52 +0000

    Lio en Date de SearchResult

---
+++ trunk/Myles/src/myles/Main.java
@@ -0,0 +1,41 @@
+package myles;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import myles.servers.*;
+import myles.config.*;
+import myles.search.*;
+import java.util.LinkedList;
+import org.jdom.Element;
+
+/**
+ *
+ * @author Juankar
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            WebUpdate upd = new WebUpdate();
+            LinkedList<Element> logins = Config.getLoginInfo();
+            Element log = logins.getLast();
+            VagosConnection v1 = new VagosConnection(log.getChildText("user"), log.getChildText("encpass"), "1");
+            int[] servers = {1, 2};
+            System.out.println("Hemos conectado? " + v1.is_connected() + " " + v1.user());
+            v1.connect();
+            System.out.println("Hemos conectado? " + v1.is_connected() + " " + v1.user());
+            SearchResult res = v1.search("prototype", servers);
+            v1.extendSearch(res);
+
+
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            System.exit(0);
+        }
+
+    }
+}
+
